97647813ad73cfcb18bce4149a4ecc17850d8f5a
Camilo Araya Olivera <camilohilda10@gmail.com>
2025-01-12 13:37:38 -0300

    borde rojo

--- assets/js/javascript1.js
+++ assets/js/javascript1.js
@@ -2,6 +2,6 @@ function toggleBorde(imagen) {
     if (imagen.style.border) {
         imagen.style.border = '';
     } else {
-        imagen.style.border = '2px solid black'; 
+        imagen.style.border = '2px solid red'; 
     }
 }
